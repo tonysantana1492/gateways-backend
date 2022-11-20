9d79f1305d3b8a8a0cbc0c7bd1769576a83cef75
Tony Santana Lopez <tonysantana1492@gmail.com>
2022-11-19 22:57:01 -0500

    Create unit and integration tests

---
+++ src/test/gateways.test.js
@@ -0,0 +1,184 @@
+const request = require("supertest");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const db_url = process.env.DB_URL;
+const db_name = process.env.DB_NAME;
+const url = `${db_url}/${db_name}`;
+const app = require("../app");
+let idGateway;
+
+/**
+ * Initial gateway object.
+ */
+const gateway = {
+  serial: Math.random(1, 200),
+  name: "Gateway 1",
+  ipv4: "127.0.0.1",
+};
+
+/**
+ * Connecting to database
+ */
+beforeAll(async () => {
+  await mongoose.connect(url);
+});
+
+/**
+ * Closing to database
+ */
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+describe("POST: /api/gateways", () => {
+  it("POST: Insert gateway successfully", async () => {
+    const response = await request(app).post("/api/gateways").send(gateway);
+    expect(response.statusCode).toBe(201);
+  });
+
+  it("POST: Error inserting gateway with same serial", async () => {
+    const response = await request(app).post("/api/gateways").send(gateway);
+    expect(response.statusCode).toBe(400);
+    expect(JSON.parse(response.text).message).toBe(
+      "#400: Gateway validation error."
+    );
+    expect(JSON.parse(response.text).fields.serial).toBe(
+      "Gateway serial already exist."
+    );
+  });
+
+  it("POST: Error inserting gateway with no serial", async () => {
+    const response = await request(app)
+      .post("/api/gateways")
+      .send({
+        ...gateway,
+        serial: "",
+      });
+    expect(response.statusCode).toBe(400);
+    expect(JSON.parse(response.text).message).toBe(
+      "#400: Gateway validation error."
+    );
+    expect(JSON.parse(response.text).fields.serial).toBe(
+      "Gateway serial is required."
+    );
+  });
+
+  it("POST: Error inserting gateway with no ipv4", async () => {
+    const response = await request(app)
+      .post("/api/gateways")
+      .send({
+        ...gateway,
+        ipv4: "",
+      });
+    expect(JSON.parse(response.text).message).toBe(
+      "#400: Gateway validation error."
+    );
+    expect(JSON.parse(response.text).fields.ipv4).toBe("Invalid IPv4 format.");
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("POST: Error inserting gateway with no name", async () => {
+    const response = await request(app)
+      .post("/api/gateways")
+      .send({
+        ...gateway,
+        name: "",
+      });
+    expect(JSON.parse(response.text).message).toBe(
+      "#400: Gateway validation error."
+    );
+    expect(JSON.parse(response.text).fields.name).toBe(
+      "Gateway name is required."
+    );
+    expect(response.statusCode).toBe(400);
+  });
+});
+
+describe("GET: /api/gateways", () => {
+  it("should return all gateways", async () => {
+    const response = await request(app).get("/api/gateways");
+    expect(response.statusCode).toBe(200);
+    expect(response.body.error).toBe(undefined);
+    expect(response.body.length).toBeGreaterThan(0);
+    const item = response.body.find((item) => item.name === "Gateway 1");
+    idGateway = item.id;
+    expect(item.name).toBe("Gateway 1");
+  });
+
+  it("GET: Recieve specific gateway", async () => {
+    const response = await request(app).get(`/api/gateways/${idGateway}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.error).toBe(undefined);
+  });
+
+  it("GET: Error fetching non-existent gateway", async () => {
+    const response = await request(app).get("/api/gateways/anynumber");
+    expect(response.statusCode).toBe(404);
+  });
+});
+
+describe("PUT /api/gateways/:idGateway", () => {
+  it("Updating specific gateway successfully", async () => {
+    const response = await request(app)
+      .put(`/api/gateways/${idGateway}`)
+      .send({
+        ...gateway,
+        name: "Gateway Change Name",
+      });
+    expect(response.statusCode).toBe(200);
+  });
+
+  it("PUT: Error updating gateway with no serial", async () => {
+    const response = await request(app)
+      .put(`/api/gateways/${idGateway}`)
+      .send({
+        ...gateway,
+        serial: "",
+      });
+    expect(response.statusCode).toBe(400);
+    expect(JSON.parse(response.text).message).toBe(
+      "#400: Gateway validation error."
+    );
+    expect(JSON.parse(response.text).fields.serial).toBe(
+      "Gateway serial is required."
+    );
+  });
+
+  it("PUT: Error updating gateway with no ipv4", async () => {
+    const response = await request(app)
+      .put(`/api/gateways/${idGateway}`)
+      .send({
+        ...gateway,
+        ipv4: "",
+      });
+    expect(JSON.parse(response.text).message).toBe(
+      "#400: Gateway validation error."
+    );
+    expect(JSON.parse(response.text).fields.ipv4).toBe("Invalid IPv4 format.");
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("PUT: Error updating gateway with no name", async () => {
+    const response = await request(app)
+      .put(`/api/gateways/${idGateway}`)
+      .send({
+        ...gateway,
+        name: "",
+      });
+    expect(JSON.parse(response.text).message).toBe(
+      "#400: Gateway validation error."
+    );
+    expect(JSON.parse(response.text).fields.name).toBe(
+      "Gateway name is required."
+    );
+    expect(response.statusCode).toBe(400);
+  });
+});
+
+describe("DELETE /api/gateways/:idGateway", () => {
+  it("should return all gateways", async () => {
+    const response = await request(app).delete(`/api/gateways/${idGateway}`);
+    expect(response.statusCode).toBe(204);
+  });
+});
---
+++ src/test/peripheral.test.js
@@ -0,0 +1,137 @@
+const request = require("supertest");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// Defines the variables
+const db_url = process.env.DB_URL;
+const db_name = process.env.DB_NAME;
+const url = `${db_url}/${db_name}`;
+const app = require("../app");
+let idGateway;
+let idPeripheral;
+
+/**
+ * Initial gateway object.
+ */
+const gateway = {
+  serial: Math.random(1, 200),
+  name: "Gateway 1",
+  ipv4: "127.0.0.1",
+};
+
+const peripheral = {
+  uid: 123,
+  vendor: "Musala",
+  status: false,
+};
+
+/**
+ * Connecting to database before each test.
+ */
+beforeAll(async () => {
+  await mongoose.connect(url);
+
+  await request(app).post("/api/gateways").send(gateway);
+
+  const responseA = await request(app).get("/api/gateways");
+  const item = responseA.body.find((item) => item.name === "Gateway 1");
+  idGateway = item.id;
+});
+
+/**
+ * Closing to database after each test.
+ */
+afterAll(async () => {
+  await request(app).delete(`/api/gateways/${idGateway}`);
+  await mongoose.connection.close();
+});
+
+describe("POST: /api/gateways/:id/peripherals", () => {
+  it("POST: Insert peripheral successfully", async () => {
+    const response = await request(app)
+      .post(`/api/gateways/${idGateway}/peripherals`)
+      .send(peripheral);
+    expect(response.statusCode).toBe(201);
+  });
+
+  it("POST: Error inserting peripheral with no uid", async () => {
+    const response = await request(app)
+      .post(`/api/gateways/${idGateway}/peripherals`)
+      .send({
+        ...peripheral,
+        uid: null,
+      });
+    expect(response.statusCode).toBe(400);
+    expect(JSON.parse(response.text).message).toBe(
+      "#400: Peripheral validation error."
+    );
+    expect(JSON.parse(response.text).fields.uid).toBe(
+      "Peripheral uid is required."
+    );
+  });
+
+  //   POST: Error inserting peripheral with no vendor
+  it("POST: Error inserting peripheral with no vendor", async () => {
+    const response = await request(app)
+      .post(`/api/gateways/${idGateway}/peripherals`)
+      .send({
+        ...peripheral,
+        vendor: "",
+      });
+    expect(JSON.parse(response.text).message).toBe(
+      "#400: Peripheral validation error."
+    );
+    expect(JSON.parse(response.text).fields.vendor).toBe(
+      "Peripheral vendor is required."
+    );
+    expect(response.statusCode).toBe(400);
+  });
+
+  //  POST: Error inserting more than 10 peripherals
+  it("POST: Error inserting more than 10 peripherals", async () => {
+    let response;
+    for (let i = 0; i < 11; i++) {
+      response = await request(app)
+        .post(`/api/gateways/${idGateway}/peripherals`)
+        .send(peripheral);
+    }
+
+    expect(JSON.parse(response.text).message).toBe(
+        "#400: Peripheral limit exceeded."
+    );   
+    expect(response.statusCode).toBe(400);
+  });
+
+  // it("POST: Error inserting peripheral with no status", async () => {
+  //   const response = await request(app)
+  //   .post(`/api/gateways/${idGateway}/peripherals`)
+  //     .send({
+  //       ...peripheral,
+  //       status: null,
+  //     });
+  //   expect(JSON.parse(response.text).message).toBe(
+  //     "#400: Peripheral validation error."
+  //   );
+  //   expect(JSON.parse(response.text).fields.status).toBe(
+  //     "Peripheral status is required."
+  //   );
+  //   expect(response.statusCode).toBe(400);
+  // });
+});
+
+describe("GET: /api/gateways/:idGateway/peripherals", ()=>{
+    it("GET: Insert peripheral successfully", async () => {
+        const response = await request(app)
+          .get(`/api/gateways/${idGateway}/peripherals`);
+          idPeripheral = response.body.peripherals.find(item => item.uid === peripheral.uid)._id;
+        expect(response.statusCode).toBe(200);
+      });
+});
+
+describe("DELETE: /api/gateways/:idGateway/peripherals/:idPeripheral", ()=>{
+    it("DELETE: delete a peripheral successfully", async () => {
+        const response = await request(app)
+          .delete(`/api/gateways/${idGateway}/peripherals/${idPeripheral}`)
+        expect(response.statusCode).toBe(204);
+      });
+});
